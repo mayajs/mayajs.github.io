6a207dc8240f664dc17ff9a97c9c85e4b40048d8
Mack <mack_ignacio@yahoo.com>
2020-03-12 22:41:50 +0800

    Generate component and added inputs to html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SidenavComponent } from './layouts/sidenav/sidenav.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SectionComponent } from './components/section/section.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SectionComponent } from './components/section/section.component';
     SidenavComponent,
     NotFoundComponent,
     SectionComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
