74241f8346b7c073343008efcf0841dd71e40bdd
Mac Ignacio <mack_ignacio@yahoo.com>
2020-03-12 22:51:45 +0800

    Chore: Update app module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidenavComponent } from './layouts/sidenav/sidenav.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { SectionComponent } from './components/section/section.component';
-import { HomeComponent } from './home/home.component';
+// MODULES
+import { AppRoutingModule } from "./app-routing.module";
+
+// COMPONENTS
+import { AppComponent } from "./app.component";
+import { SidenavComponent } from "./components/sidenav/sidenav.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { SectionComponent } from "./components/section/section.component";
+import { HomeComponent } from "./components/home/home.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     NotFoundComponent,
-    SectionComponent
+    SectionComponent,
     HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
