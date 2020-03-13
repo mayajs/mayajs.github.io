3f483500ec05d63771f20c1776b2ed476fb8f6d0
kanutodev <rodenjakev@gmial.com>
2020-03-13 16:28:33 +0800

    added header component for layout, second try

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  HeaderLink = [
+    {name: "features", link: "maya"},
+    {name: "docs", link: "get-start"},
+    {name: "resources", link: "resources"},
+    {name: "events", link: "events"}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  private val = 0
+
+  toggle() {
+    if(this.val == 0) {
+      document.getElementById("sidenav").style.width = "0";
+      document.getElementById("main-content").style.marginLeft = "0";
+      this.val = 1;
+    } else {
+      document.getElementById("sidenav").style.width = "250px";
+      document.getElementById("main-content").style.marginLeft = "250px";
+      this.val = 0;
+    }
+  }
+
+}
