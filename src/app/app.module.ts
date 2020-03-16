52accbfebbf22ec4c0a75a30a7585c30d379a356
Joshua Seras <joshuacretencioseras@gmail.com>
2020-03-16 16:44:00 +0800

    FEAT : add intro section fundementals content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { SectionComponent } from "./components/section/section.component";
 import { HomeComponent } from "./components/home/home.component";
 import { BasicConceptsComponent } from './components/contents/sidenav/fundementals/maya-concept/basic-concepts/basic-concepts.component';
+import { MayaConceptSectionComponent } from './components/section/sidenav/fundementals/maya-concept/maya-concept-section/maya-concept-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoalsComponent,
     FeaturesComponent,
-    BasicConceptsComponent
+    BasicConceptsComponent,
+    MayaConceptSectionComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/components/section/sidenav/fundementals/maya-concept/maya-concept-section/maya-concept-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maya-concept-section',
+  templateUrl: './maya-concept-section.component.html',
+  styleUrls: ['./maya-concept-section.component.scss']
+})
+export class MayaConceptSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
