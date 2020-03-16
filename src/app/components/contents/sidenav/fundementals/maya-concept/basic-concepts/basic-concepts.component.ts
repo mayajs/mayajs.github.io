5e93f4d45d3d2a0658df5f9c195abbde8708ec27
Joshua Seras <joshuacretencioseras@gmail.com>
2020-03-16 16:41:34 +0800

    FEAT : add fundementals sidenav contents

---
+++ src/app/components/contents/sidenav/fundementals/maya-concept/basic-concepts/basic-concepts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-concepts',
+  templateUrl: './basic-concepts.component.html',
+  styleUrls: ['./basic-concepts.component.scss']
+})
+export class BasicConceptsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
