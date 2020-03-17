c4f346fd1748eaeb6b91bdf1748980e8a5eecd5b
Mack <mack_ignacio@yahoo.com>
2020-03-17 19:15:17 +0800

    CHORE: Add header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  HeaderLink = [
+    {name: "features", link: "maya"},
+    {name: "docs", link: "get-start"},
+    {name: "resources", link: "resources"},
+    {name: "events", link: "events"}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  private val = 0
+
+  toggle() {
+    if(this.val == 0) {
+      document.getElementById("sidenav").style.width = "0";
+      document.getElementById("main-content").style.marginLeft = "0";
+      this.val = 1;
+    } else {
+      document.getElementById("sidenav").style.width = "250px";
+      document.getElementById("main-content").style.marginLeft = "250px";
+      this.val = 0;
+    }
   }
 
 }
