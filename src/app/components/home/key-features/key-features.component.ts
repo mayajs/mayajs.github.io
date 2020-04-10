92c13e1eea6d968afce7d2529383adf18f721d6d
Mac Ignacio <mack_ignacio@yahoo.com>
2020-04-11 07:58:41 +0800

    FEAT: Add svg url strings

--- src/app/components/home/key-features/key-features.component.ts
+++ src/app/components/home/key-features/key-features.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./key-features.component.scss"],
 })
 export class KeyFeaturesComponent implements OnInit {
+  svg = {
+    ts: "../../../../assets/svg/typescript.svg",
+    express: "../../../../assets/svg/routing.svg",
+    code: "../../../../assets/svg/code.svg",
+    setting: "../../../../assets/svg/settings.svg",
+    check: "../../../../assets/svg/check.svg",
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
