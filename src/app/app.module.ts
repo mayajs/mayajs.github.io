fb7aa7c44eed4d0ea0263759b0e4fe9e53cdd26a
Mac Ignacio <mack_ignacio@yahoo.com>
2020-04-11 04:46:51 +0800

    FIX: Component declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,35 @@ import { GoalsComponent } from "./components/contents/goals/goals.component";
 import { FeaturesComponent } from "./components/contents/features/features.component";
 
 // COMPONENTS
+import { AppComponent } from "./app.component";
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { SectionComponent } from "./components/section/section.component";
 import { HomeComponent } from "./components/home/home.component";
-import { IntroductionComponent } from './components/sidenav/introduction/introduction.component';
-import { SetupInstallationComponent } from './components/section/setup-installation/setup-installation.component';
+import { IntroductionComponent } from "./components/sidenav/introduction/introduction.component";
+import { SetupInstallationComponent } from "./components/section/setup-installation/setup-installation.component";
+import { BasicConceptsComponent } from "./components/contents/sidenav/fundementals/maya-concept/basic-concepts/basic-concepts.component";
+import { ModulesSectionComponent } from "./components/section/sidenav/fundementals/maya-concept/modules-section/modules-section.component";
+import { ModulesContentComponent } from "./components/contents/sidenav/fundementals/maya-concept/modules-content/modules-content.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidenavComponent,
+    NotFoundComponent,
+    SectionComponent,
+    HomeComponent,
     GoalsComponent,
     FeaturesComponent,
     BasicConceptsComponent,
-    MayaConceptSectionComponent,
     ModulesSectionComponent,
     ModulesContentComponent,
     FeaturesComponent,
     IntroductionComponent,
     SetupInstallationComponent,
-
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
