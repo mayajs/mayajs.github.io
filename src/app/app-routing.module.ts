908b415a9ab67183e7a1e28df8e95a8baab5b9b5
Mac Ignacio <mack_ignacio@yahoo.com>
2020-04-11 05:24:55 +0800

    FEAT: Add learn-more and faq routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { Routes, RouterModule } from "@angular/router";
 
 // COMPONENTS
 import { HomeComponent } from "./components/home/home.component";
+import { LearnMoreComponent } from "./components/learn-more/learn-more.component";
+import { FaqComponent } from "./components/faq/faq.component";
 
 const routes: Routes = [
+  { path: "learn-more", component: LearnMoreComponent },
+  { path: "faq", component: FaqComponent },
   { path: "", component: HomeComponent },
-  { path: "**", redirectTo: "", pathMatch: "full" }
+  { path: "**", redirectTo: "", pathMatch: "full" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
