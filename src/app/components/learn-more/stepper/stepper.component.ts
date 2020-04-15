44d0527dd81daf4e4e52ff1f306422fe44c4c700
Christian Cueto <cuetoc4@gmail.com>
2020-04-15 13:56:49 +0800

    FEAT: Added stepper TS animation

--- src/app/components/learn-more/stepper/stepper.component.ts
+++ src/app/components/learn-more/stepper/stepper.component.ts
@@ -10,6 +10,16 @@ export class StepperComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const allItems = document.querySelectorAll(".navmenu ul li a");
+    allItems.forEach(item => {
+    item.addEventListener('click', function(e) {
+
+        for (var i=0; i<allItems.length; i++){
+            allItems[i].classList.remove("active");
+        }
+        this.classList.add('active');
+       })
+    })
   }
 
 }
