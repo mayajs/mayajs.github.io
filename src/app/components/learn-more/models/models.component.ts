9daee31e45b6c59dd26306e527f37e3c1d5396f2
Christian Cueto <cuetoc4@gmail.com>
2020-04-17 21:16:48 +0800

    CHORE: Added the copy to clipboard fucntion

--- src/app/components/learn-more/models/models.component.ts
+++ src/app/components/learn-more/models/models.component.ts
@@ -11,6 +11,32 @@ export class ModelsComponent implements OnInit {
     copyIcon: "../../../assets/svg/copypaste-icon.svg"
   };
 
+  @ViewChild('codeVar1') dynamicRef1: ElementRef;
+  @ViewChild('codeVar2') dynamicRef2: ElementRef;
+
+
+  get codeVal() {
+    return this.dynamicRef1.nativeElement.innerHTML;
+  }
+  get codeVal2() {
+    return this.dynamicRef2.nativeElement.innerHTML;
+  }
+
+  copyCode(numVar): void {
+    var codeCopy = document.createElement("textarea");
+    document.body.appendChild(codeCopy);
+    if (numVar==1){
+      codeCopy.value = this.codeVal;
+    }
+    else
+    {
+      codeCopy.value = this.codeVal2;
+    }
+    codeCopy.select();
+    document.execCommand("copy");
+    document.body.removeChild(codeCopy);
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
