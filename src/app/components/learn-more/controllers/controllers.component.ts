dc7094e5f910280b6222b0e3981f21f0b275ded4
Christian Cueto <cuetoc4@gmail.com>
2020-04-17 17:44:46 +0800

    CHORE: Added Copy code in clipboard function

--- src/app/components/learn-more/controllers/controllers.component.ts
+++ src/app/components/learn-more/controllers/controllers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { ElementRef, ViewChild } from '@angular/core';
+
+//AfterViewInit,
 
 @Component({
   selector: "app-controllers",
@@ -6,6 +9,36 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./controllers.component.scss"],
 })
 export class ControllersComponent implements OnInit {
+  GSsvg = {
+    copyIcon: "../../../assets/svg/copypaste-icon.svg"
+  };
+
+  @ViewChild('codeVar1') dynamicRef1: ElementRef;
+  @ViewChild('codeVar2') dynamicRef2: ElementRef;
+
+
+  get codeVal() {
+    return this.dynamicRef1.nativeElement.innerHTML;
+  }
+  get codeVal2() {
+    return this.dynamicRef2.nativeElement.innerHTML;
+  }
+
+  copyCode(numVar): void {
+    var codeCopy = document.createElement("textarea");
+    document.body.appendChild(codeCopy);
+    if (numVar==1){
+      codeCopy.value = this.codeVal;
+    }
+    else
+    {
+      codeCopy.value = this.codeVal2;
+    }
+    codeCopy.select();
+    document.execCommand("copy");
+    document.body.removeChild(codeCopy);
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
