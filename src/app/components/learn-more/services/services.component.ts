bdbf36cfe7612182f123018d719ebb03822a1b4d
Christian Cueto <cuetoc4@gmail.com>
2020-04-18 12:35:30 +0800

    CHORE: Added services contents and copy to clipboard.

--- src/app/components/learn-more/services/services.component.ts
+++ src/app/components/learn-more/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: "app-services",
@@ -6,6 +7,39 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./services.component.scss"],
 })
 export class ServicesComponent implements OnInit {
+  GSsvg = {
+    copyIcon: "../../../assets/svg/copypaste-icon.svg"
+  };
+
+  @ViewChild('codeVar1') dynamicRef1: ElementRef;
+  @ViewChild('codeVar2') dynamicRef2: ElementRef;
+
+
+  get codeVal() {
+    return this.dynamicRef1.nativeElement.innerHTML;
+  }
+  get codeVal2() {
+    return this.dynamicRef2.nativeElement.innerHTML;
+  }
+
+  copyCode(numVar): void {
+    var codeCopy = document.createElement("textarea");
+    document.body.appendChild(codeCopy);
+    if (numVar==1){
+      codeCopy.value = this.codeVal;
+    }
+    else
+    {
+      codeCopy.value = this.codeVal2;
+    }
+    codeCopy.select();
+    document.execCommand("copy");
+    document.body.removeChild(codeCopy);
+  }
+
+
+
+
   constructor() {}
 
   ngOnInit(): void {}
