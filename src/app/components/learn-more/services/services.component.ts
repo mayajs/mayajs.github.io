4bf804069519bab145cf153d048c63fe333b6a8d
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-20 02:12:09 +0800

    FEAT: Add services page UI

--- src/app/components/learn-more/services/services.component.ts
+++ src/app/components/learn-more/services/services.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
 
 @Component({
   selector: "app-services",
   templateUrl: "./services.component.html",
   styleUrls: ["./services.component.scss"],
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, AfterViewInit {
+  @ViewChild("iframe") iframe: ElementRef;
+  gistId = "https://gist.github.com/Mackignacio/a30c285fae14a50cdb5986b515a9a55e.js";
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.iframe.nativeElement.id = "gist-" + this.gistId;
+    const doc = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentElement.contentWindow;
+    const content = `
+        <html>
+        <head>
+          <base target="_parent">
+        </head>
+        <script type="text/javascript" src="${this.gistId}"></script>
+        </body>
+      </html>
+    `;
+    doc.open();
+    doc.write(content);
+    doc.close();
+  }
 }
