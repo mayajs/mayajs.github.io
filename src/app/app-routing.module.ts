47fbbde0651e89b7795a7823bc1f21417e48d238
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-19 23:44:59 +0800

    FEAT: Add get-started route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { LearnMoreComponent } from "./components/learn-more/learn-more.component";
 import { FaqComponent } from "./components/faq/faq.component";
+import { GettingStartedComponent } from "./components/learn-more/getting-started/getting-started.component";
 
 const routes: Routes = [
   { path: "learn-more", component: LearnMoreComponent },
   { path: "faq", component: FaqComponent },
+  { path: "get-started", component: GettingStartedComponent },
   { path: "", component: HomeComponent },
   { path: "**", redirectTo: "", pathMatch: "full" },
 ];
