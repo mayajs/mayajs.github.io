fac5bc0554d50717693130c700a1386da053bbac
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-20 01:17:52 +0800

    FEAT: Add controllers page

--- src/app/components/learn-more/controllers/controllers.component.ts
+++ src/app/components/learn-more/controllers/controllers.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
 
 @Component({
   selector: "app-controllers",
   templateUrl: "./controllers.component.html",
   styleUrls: ["./controllers.component.scss"],
 })
-export class ControllersComponent implements OnInit {
+export class ControllersComponent implements OnInit, AfterViewInit {
+  @ViewChild("iframe") iframe: ElementRef;
+  gistId = "https://gist.github.com/Mackignacio/4d4efa1579130a8409879cb1ddac07f0.js";
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.iframe.nativeElement.id = "gist-" + this.gistId;
+    const doc = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentElement.contentWindow;
+    const content = `
+        <html>
+        <head>
+          <base target="_parent">
+        </head>
+        <script type="text/javascript" src="${this.gistId}"></script>
+        </body>
+      </html>
+    `;
+    doc.open();
+    doc.write(content);
+    doc.close();
+  }
 }
