b7505c4a29a8ea4479f5718149a7e95e5cc9866b
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-24 18:28:33 +0800

    FEAT: Add simpleList array

--- src/app/components/why-mayajs/why-mayajs.component.ts
+++ src/app/components/why-mayajs/why-mayajs.component.ts
@@ -11,6 +11,14 @@ export class WhyMayajsComponent implements OnInit {
     "Can be used by developers with any skill levels.",
     "Help in making complex routes by using built-in cli.",
   ];
+
+  simpleList = [
+    "Declarative way of defining routes.",
+    "Adding route files has gotten easy using our CLI.",
+    "Business logic can be shared using services.",
+    "Less runtime error with type checking using Typescript.",
+    "Create method routes using method declaration.",
+  ];
   constructor() {}
 
   ngOnInit(): void {}
