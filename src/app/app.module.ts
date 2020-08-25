ead4a2b5b54d90c214b94bde4e89c8c6293504f3
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-26 02:04:17 +0800

    CHORE: Remove controller component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { MissionComponent } from "./components/mission/mission.component";
 import { VisionComponent } from "./components/vision/vision.component";
 import { GettingStartedComponent } from "./components/getting-started/getting-started.component";
 import { RoutesComponent } from "./components/routes/routes.component";
-import { ControllersComponent } from "./components/controllers/controllers.component";
 import { ModelsComponent } from "./components/models/models.component";
 import { ServicesComponent } from "./components/services/services.component";
 import { GistComponent } from "./components/gist/gist.component";
@@ -27,7 +26,6 @@ import { WhyMayajsComponent } from "./components/why-mayajs/why-mayajs.component
     VisionComponent,
     GettingStartedComponent,
     RoutesComponent,
-    ControllersComponent,
     ModelsComponent,
     ServicesComponent,
     GistComponent,
