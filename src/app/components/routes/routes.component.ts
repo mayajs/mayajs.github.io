021258290d569837a4d48bc3c27ef94bcd3c750e
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-26 01:49:21 +0800

    FEAT: Add createCode function

--- src/app/components/routes/routes.component.ts
+++ src/app/components/routes/routes.component.ts
@@ -6,22 +6,27 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from "@angula
   styleUrls: ["./routes.component.scss"],
 })
 export class RoutesComponent implements OnInit, AfterViewInit {
-  @ViewChild("iframe") iframe: ElementRef;
-  gistId = "https://gist.github.com/Mackignacio/f635530aef64df0305dda7c4cf4a916b.js";
+  @ViewChild("controller") controllerFrame: ElementRef;
+  controllerId = "https://gist.github.com/Mackignacio/4d4efa1579130a8409879cb1ddac07f0.js";
 
   constructor() {}
 
   ngOnInit(): void {}
 
   ngAfterViewInit() {
-    this.iframe.nativeElement.id = "gist-" + this.gistId;
-    const doc = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentElement.contentWindow;
+    const codes = [{ iframe: this.controllerFrame, id: this.controllerId }];
+    codes.forEach(code => this.createCode(code));
+  }
+
+  createCode(code: { iframe: ElementRef<any>; id: string }) {
+    code.iframe.nativeElement.id = "gist-" + code.id;
+    const doc = code.iframe.nativeElement.contentDocument || code.iframe.nativeElement.contentElement.contentWindow;
     const content = `
         <html>
         <head>
           <base target="_parent">
         </head>
-        <script type="text/javascript" src="${this.gistId}"></script>
+        <script type="text/javascript" src="${code.id}"></script>
         </body>
       </html>
     `;
