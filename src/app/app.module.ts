7d377f7d7b9cc771540016f3e7f83ba5f38e785c
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-26 02:38:57 +0800

    CHORE: Remove models component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { MissionComponent } from "./components/mission/mission.component";
 import { VisionComponent } from "./components/vision/vision.component";
 import { GettingStartedComponent } from "./components/getting-started/getting-started.component";
 import { RoutesComponent } from "./components/routes/routes.component";
-import { ModelsComponent } from "./components/models/models.component";
 import { ServicesComponent } from "./components/services/services.component";
 import { GistComponent } from "./components/gist/gist.component";
 import { WhyMayajsComponent } from "./components/why-mayajs/why-mayajs.component";
@@ -26,7 +25,6 @@ import { WhyMayajsComponent } from "./components/why-mayajs/why-mayajs.component
     VisionComponent,
     GettingStartedComponent,
     RoutesComponent,
-    ModelsComponent,
     ServicesComponent,
     GistComponent,
     WhyMayajsComponent,
